19a32e6e21ecbba642e8128d00b7d129c5dbb1dc
Shabana Nazeer <nizam111106@gmail.com>
2025-10-22 13:51:57 +0530

    Added findByEmail method to UserRepository

--- backend/src/main/java/com/nizam/jobportal/repository/UserRepository.java
+++ backend/src/main/java/com/nizam/jobportal/repository/UserRepository.java
@@ -2,5 +2,11 @@ package com.nizam.jobportal.repository;
 
 import com.nizam.jobportal.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // ✅ Add this line if missing
+    User findByEmail(String email);
+}
